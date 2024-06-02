520caab21dc8546c35a29e089b482e6ead8a4ae8
vanya992 <111580069+vanya992@users.noreply.github.com>
2024-06-02 13:33:36 +0200

    footer style applied

--- src/Components/Layout/Footer/index.jsx
+++ src/Components/Layout/Footer/index.jsx
@@ -1,3 +1,10 @@
+import styles from "./Footer.module.css";
+
 export function Footer() {
-  return <footer> Vladimira Dmitrovic - Frontend Developer 2024</footer>;
+  return (
+    <footer className={styles.footer}>
+      {" "}
+      Vladimira Dmitrovic - Frontend Developer 2024
+    </footer>
+  );
 }
